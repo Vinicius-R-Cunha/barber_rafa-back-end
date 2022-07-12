ff5459c4668237e308718b4f363f8b313d909a7e
Vinicius <vinicunha2008@gmail.com>
2022-07-12 11:51:40 -0300

    oAuth logs test

--- src/controllers/oAuthController.ts
+++ src/controllers/oAuthController.ts
@@ -4,7 +4,9 @@ import * as oAuthService from "../services/oAuthService.js";
 export async function handleOAuth(req: Request, res: Response) {
   const { oAuthType } = req.params;
 
+  console.log("chegou aqui");
   const response = await oAuthService.handleOAuth(req.body, oAuthType);
+  console.log(response);
 
   res.status(200).send(response);
 }
