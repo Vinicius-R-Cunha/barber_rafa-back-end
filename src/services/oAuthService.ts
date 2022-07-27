93cc868bfcbdfb280dcf01d4e4a4f36691f97eac
Vinicius <vinicunha2008@gmail.com>
2022-07-27 03:06:50 -0300

    function refactor

--- src/services/oAuthService.ts
+++ src/services/oAuthService.ts
@@ -12,95 +12,47 @@ export interface oAuthData {
 export type TokenData = Omit<oAuthData, "id">;
 
 export async function handleOAuth(body: oAuthData, oAuthType: string) {
-  const bodyStrip = {
+  const { id, name, email, phone } = {
     id: strip(body.id),
     name: strip(body.name),
     email: strip(body.email),
     phone: strip(body.phone),
   };
-  if (oAuthType === "facebook") {
-    let token: string;
 
-    const user = await userRepository.findByEmail(bodyStrip.email);
+  let token: string;
+  const user = await userRepository.findByEmail(email);
 
-    if (!user) {
-      await userRepository.createNewFacebookUser(
-        bodyStrip.id,
-        bodyStrip.name,
-        bodyStrip.email,
-        bodyStrip.phone
-      );
-
-      token = generateToken({
-        name: bodyStrip.name,
-        email: bodyStrip.email,
-        phone: bodyStrip.phone,
-      });
-
-      return { token, newUser: true };
+  if (!user) {
+    if (oAuthType === "facebook") {
+      await userRepository.createNewFacebookUser(id, name, email, phone);
+    } else if (oAuthType === "google") {
+      await userRepository.createNewGoogleUser(id, name, email, phone);
+    } else {
+      throw { type: "bad_request", message: "Tipo de login inválido" };
     }
 
-    if (!user.facebookId) {
-      throw {
-        type: "conflict",
-        message:
-          "Já existe uma conta associada à esse email, tente fazer login normalmente!",
-      };
-    }
+    token = generateToken({ name, email, phone });
 
-    token = generateToken({
-      name: user.name,
-      email: user.email,
-      phone: user.phone,
-    });
-
-    if (user.phone.length < 15) return { token, newUser: true };
-
-    return { token, newUser: false };
+    return { token, newUser: true };
   }
 
-  if (oAuthType === "google") {
-    let token: string;
-
-    const user = await userRepository.findByEmail(bodyStrip.email);
-
-    if (!user) {
-      await userRepository.createNewGoogleUser(
-        bodyStrip.id,
-        bodyStrip.name,
-        bodyStrip.email,
-        bodyStrip.phone
-      );
-
-      token = generateToken({
-        name: bodyStrip.name,
-        email: bodyStrip.email,
-        phone: bodyStrip.phone,
-      });
-
-      return { token, newUser: true };
-    }
-
-    if (!user.googleId) {
-      throw {
-        type: "conflict",
-        message:
-          "Já existe uma conta associada à esse email, tente fazer login normalmente!",
-      };
-    }
-
-    token = generateToken({
-      name: user.name,
-      email: user.email,
-      phone: user.phone,
-    });
-
-    if (user.phone.length < 15) return { token, newUser: true };
-
-    return { token, newUser: false };
+  if (!user.facebookId && !user.googleId) {
+    throw {
+      type: "conflict",
+      message:
+        "Já existe uma conta associada à esse email, tente fazer login normalmente!",
+    };
   }
 
-  throw { type: "bad_request", message: "Tipo de login inválido" };
+  token = generateToken({
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+  });
+
+  if (user.phone.length < 15) return { token, newUser: true };
+
+  return { token, newUser: false };
 }
 
 function generateToken(data: TokenData) {
